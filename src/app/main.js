54456c0b153773b2f15330664a60ad4420db9c5c
sm <sp3ber@Gmail.com>
2016-08-23 10:23:32 +0400

    add ng-redux

--- src/app/main.js
+++ src/app/main.js
@@ -3,21 +3,17 @@ export const main = {
   template: require('./main.html'),
   controller: function ($ngRedux, $scope) {
     /** @ngInject */
-
+    this.todos = [];
     this.$onInit = function() {
       //this.getAsyncTodos();
-      console.log(this.todos);
     };
-
-    this.todos = [];
-
-    const unsubscribe = $ngRedux.connect(this.mapStateToThis, todoActions)(this);
-    $scope.$on('$destroy', unsubscribe);
 
     this.mapStateToThis = function (state) {
       return {
         todos: state.todos
       };
     };
+    const unsubscribe = $ngRedux.connect(this.mapStateToThis, todoActions)(this);
+    $scope.$on('$destroy', unsubscribe);
   }
 };
