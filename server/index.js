733cd636a04ed7155f7947f856bb32c6f3e35a33
sm <sp3ber@Gmail.com>
2016-08-29 01:21:41 +0400

    build for heroku

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var staticPath = path.join(__dirname, '/../public');
+var staticPath = path.join(__dirname, '/../dist');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(staticPath));
